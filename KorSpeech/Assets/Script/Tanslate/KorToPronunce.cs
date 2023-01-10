b0d41483c410f15f0f33a4313408c434dec2063c
husk <huskgit@gmail.com>
2023-01-10 20:59:28 +0900

    feat : TDD 맞춰 한글 자모분리 알고리즘 구현

---
+++ KorSpeech/Assets/Script/Tanslate/KorToPronunce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KorToPronunce : MonoBehaviour
+{
+    public void InputWord(string word) 
+    {
+
+    }
+
+    public string OutputPronunciation()
+    {
+        return "a";
+    }
+
+    private void OneWordToPronunciation()
+    {
+
+    }
+}
---
+++ KorSpeech/Assets/Script/Tanslate/SliceKorWord.cs
@@ -0,0 +1,44 @@
+using System.Text;
+using KorSpeech.Translate.KoreanData;
+
+namespace KorSpeech.Translate
+{
+    public class SliceKorWord : IWordToPronunce
+    {
+        private readonly int KOR_UNICODE_START = 0xAC00;  
+        private readonly int KOR_UNICODE_END = 0xD7AF;
+
+        public string StringToPronunciation(string input)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == ' ')
+                {
+                    result.Append(' ');
+                    continue;
+                }
+                SliceOneKorWord(input[i], result);
+            }
+
+            return result.ToString();
+        }
+
+        private void SliceOneKorWord(char word, StringBuilder array)
+        {
+            if(word > KOR_UNICODE_END || word < KOR_UNICODE_START)    return;
+            int indexGap = word - KOR_UNICODE_START;
+
+            int first = indexGap / 21 / 28;
+            int mid  = (indexGap - (first * 21 * 28)) / 28;
+            int end   = indexGap - (first * 21 * 28) - (mid * 28) - 1;
+
+            array.Append(KoreanSeong.firstSeong[first]);
+            array.Append(KoreanSeong.midSeong[mid]);
+
+            if(end > 0)
+                array.Append(KoreanSeong.endSeong[end]);
+        }
+    }
+}
