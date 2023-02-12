8223fa06e658715bf7b3fc82aa3375713bb24cd9
husk <huskgit@gmail.com>
2023-02-12 16:36:02 +0900

    feat : 소리 동화 일부 추가

--- KorSpeech/Assets/Script/Tanslate/KoreanToPronunce.cs
+++ KorSpeech/Assets/Script/Tanslate/KoreanToPronunce.cs
@@ -34,13 +34,35 @@ namespace KorSpeech.Translate
             int first = indexGap / 21 / 28;
             int mid  = (indexGap - (first * 21 * 28)) / 28;
             int end   = indexGap - (first * 21 * 28) - (mid * 28) - 1;
-
-            if(first != 11) // 'ㅇ' 은 제거하기
-                array.Append(KoreanSeong.firstSeong[first]);
+            array.Append(KoreanSeong.firstSeong[first]);
             array.Append(KoreanSeong.midSeong[mid]);
 
             if(end >= 0)
                 array.Append(KoreanSeong.endSeong[end]);
+        }
+
+        private void ConsonantAssimiliation(StringBuilder array)
+        {
+            for(int i = 0; i < array.Length - 2; i++)
+            {
+                if(array[i + 1] == 'ㅇ' && array[i + 2] == 'ㅣ')
+                {
+                    if(array[i] == 'ㄷ')
+                    {
+                        array[i] = 'ㅈ';
+                    }
+                    if(array[i] == 'ㅌ' || array[i] == 'ㄾ')
+                    {
+                        array[i] = 'ㅊ';
+                    }
+                    continue;
+                }
+                if(array[i] == 'ㅁ' || array[i] == 'ㅇ' || array[i] == 'ㄱ' || array[i] == 'ㅂ')
+                {
+                    if(array[i + 1] == 'ㄹ')
+                        array[i + 1] = 'ㄴ';
+                }
+            }
         }
     }
 }
